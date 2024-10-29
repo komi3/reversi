5efdc5d001a165c3a5e54341824893b173da67ae
micha <michal-martan@seznam.cz>
2024-10-29 12:06:12 +0100

    drawing player moves

--- main.py
+++ main.py
@@ -7,20 +7,44 @@ SCREEN_SIZE = 800
 BOARD_SIZE = 8
 SQUARE_SIZE = SCREEN_SIZE // BOARD_SIZE
 WHITE, BLACK, EMPTY = 1, -1, 0
-BG_COLOR = (0, 128, 0)
-WHITE_COLOR = (255, 255, 255)
-BLACK_COLOR = (0, 0, 0)
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
 pygame.display.set_caption('Reversi')
 
+class Board:
+    def __init__(self):
+        BOARD_SIZE = 8
+        self.grid = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=int)
 
-def draw_board():
-    screen.fill(BG_COLOR)
-    for row in range(BOARD_SIZE):
-        for col in range(BOARD_SIZE):
-            pygame.draw.rect(screen, (0, 0, 0), (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 1)
+        self.grid[3, 3] = WHITE
+        self.grid[3, 4] = BLACK
+        self.grid[4, 3] = BLACK
+        self.grid[4, 4] = WHITE
+    def draw_board(self):
+        screen.fill((0, 128, 0))
+
+        screen.fill((0, 128, 0))
+        for row in range(BOARD_SIZE):
+            for col in range(BOARD_SIZE):
+                pygame.draw.rect(screen, (0, 0, 0), (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 1)
+                if self.grid[row, col] == WHITE:
+                    pygame.draw.circle(screen, (255, 255, 255),
+                                       (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2),
+                                       SQUARE_SIZE // 2 - 5)
+
+                elif self.grid[row, col] == BLACK:
+                    pygame.draw.circle(screen, (0, 0, 0),
+                                       (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2),
+                                       SQUARE_SIZE // 2 - 5)
+
+
+
+
+
+
+board = Board()
+current_turn = BLACK
 
 while True:
     for event in pygame.event.get():
@@ -29,12 +53,28 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-                print("Button clicked!")
-                print( event.pos)
+                mouse_x, mouse_y = event.pos
+                row, col = mouse_y // SQUARE_SIZE, mouse_x // SQUARE_SIZE
 
-    # Fill the screen with white
-    draw_board()
+                if current_turn == WHITE:
+                    board.grid[row, col] = WHITE
+                    current_turn = BLACK
+                    print(board.grid)
+                    print(current_turn)
+
+                elif current_turn == BLACK:
+                    board.grid[row, col] = BLACK
+                    current_turn = WHITE
+                    print(board.grid)
+                    print(current_turn)
+
+
+    board.draw_board()
+
 
 
     # Update the display
     pygame.display.flip()
+
+
+
